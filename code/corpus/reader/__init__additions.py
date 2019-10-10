0956029129b1835344c82f4904bbd8747cb05534
Colin Goldberg <2991417+colingoldberg@users.noreply.github.com>
2019-10-10 16:44:04 -0400

    Create __init__additions.py
    Snippet for st_wordnet.py only

---
+++ code/corpus/reader/__init__additions.py
@@ -0,0 +1,8 @@
+# Add the import statement near the wordnet import statement
+
+from nltk.corpus.reader.st_wordnet import *
+
+
+# Add an entry near the 'WordNetCorpusReader' line
+
+    'StWordNetCorpusReader',
